d477d4560cf29a687585a6f786805d9f4a8bea60
Saber Samadi <saber_samadi@outlook.com>
2026-04-13 15:19:11 +0200

    new commits

---
+++ person.java
@@ -0,0 +1,12 @@
+public class person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+}
